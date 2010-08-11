207e4f2a29ec8c4485edb48d7444b5f797941fe9
Jochen Issing <c.333+github@nesono.com>
2010-08-11 15:46:07 +0200

    added test for transfer stack - not yet complete :/

--- test_transferstack.sh
+++ test_transferstack.sh
@@ -1,51 +1,75 @@
 #!/bin/bash
 # script to test transfer stack functionality
 
+. bashrc
+
 # uses the following functions:
 #
 # transfers
 # pusht
 # popt
 
+function debug()
+{
+  #echo $*
+  return 1
+}
+
 TESTDIR=/tmp/transstacktest.${RANDOM}
 /bin/rm -rf ${TESTDIR}
 echo "using testdir: \"${TESTDIR}\""
 # create tmp directory and change into it
-mkdir -p "${TESTDIR}" || echo "could not create testdir" && exit 1
+mkdir -p "${TESTDIR}"    || echo "could not create testdir"
 # change into test dir
-cd "${TESTDIR}" || echo "could not change into testdir" && exit 2
+cd "${TESTDIR}"          || echo "could not change into testdir"
+
+debug "i am here: $(pwd)"
+debug "contents: " && ls -lR ${TESTDIR}
 
 # create some directories
-mkdir sub1 sub2 || echo "could not create subdirs" && exit 3
+mkdir sub1 sub2          || echo "could not create subdirs"
 # change into sub1
-cd sub1 || echo "could not change into sub1" && exit 4
+cd sub1                  || echo "could not change into sub1"
+
+debug "i am here: $(pwd)"
+debug "contents: " && ls -lR ${TESTDIR}
+
 # create some files
-echo "test1" > test1.txt || echo "could not create testfile 1" && exit 5
-echo "test2" > test2.txt || echo "could not create testfile 2" && exit 5
-echo "test3" > test3.txt || echo "could not create testfile 3" && exit 5
+echo "test1" > test1.txt || echo "could not create testfile 1"
+echo "test2" > test2.txt || echo "could not create testfile 2"
+echo "test3" > test3.txt || echo "could not create testfile 3"
+
+debug "contents: " && ls -lR ${TESTDIR}
 
 # change into differen subdir
-cd ../sub2 || echo "could not change into sub2" && exit 6
+cd ../sub2 || echo "could not change into sub2"
+
+debug "i am here: $(pwd)"
+debug "contents: " && ls -lR ${TESTDIR}
+
 # push all created test files 3 times with different modes
-pusht mv ../sub1/test1
-pusht mv ../sub1/test2
+pusht mv ../sub1/test1.txt
+pusht mv ../sub1/test2.txt
 
-pusht cp ../sub1/test1
-pusht cp ../sub1/test2
-pusht cp ../sub1/test3
+pusht cp ../sub1/test1.txt
+pusht cp ../sub1/test2.txt
+pusht cp ../sub1/test3.txt
 
-pusht ../sub1/test1
-pusht ../sub1/test2
-pusht ../sub1/test3
+pusht ../sub1/test1.txt
+pusht ../sub1/test2.txt
+pusht ../sub1/test3.txt
 
-pusht ../sub1/test1
-pusht ../sub1/test2
-pusht ../sub1/test3
+pusht ../sub1/test1.txt
+pusht ../sub1/test2.txt
+pusht ../sub1/test3.txt
 
 # check transfers
-[ "$(transfers | wc -l)" != 11 ] || echo "transfer list size 1 bad!" && exit 7
+[ "$(transfers | wc -l)" != 11 ] || echo "transfer list size 1 bad!"
 # change into other dir
-cd ../sub2 || echo "could not change into sub2" && exit 7
+cd ../sub2               || echo "could not change into sub2"
+
+debug "i am here: $(pwd)"
+debug "contents: " && ls -lR ${TESTDIR}
 
 # call first 3 items (test[1-3])
 popt
@@ -64,25 +88,34 @@ popt
 # pusht ../sub1/test2
 # pusht ../sub1/test3
 
+debug "i am here: $(pwd)"
+debug "contents: " && ls -lR ${TESTDIR}
+
 # check transfers
-[ "$(transfers | wc -l)" != 8 ] || echo "transfer list size 2 bad!" && exit 7
+[ "$(transfers | wc -l)" != 8 ] || echo "transfer list size 2 bad!"
 
 # check, if files here
-[ -e ./test1 ] || echo "test1 file missing" && exit 8
-[ -e ./test2 ] || echo "test2 file missing" && exit 8
-[ -e ./test3 ] || echo "test3 file missing" && exit 8
+[ -e ./test1.txt ]           || echo "test1 file missing"
+[ -e ./test2.txt ]           || echo "test2 file missing"
+[ -e ./test3.txt ]           || echo "test3 file missing"
 # check, if source files still there
-[ -e ../sub1/test1 ] || echo "test1 source file missing" && exit 9
-[ -e ../sub1/test2 ] || echo "test2 source file missing" && exit 9
-[ -e ../sub1/test3 ] || echo "test3 source file missing" && exit 9
+[ -e ../sub1/test1.txt ]     || echo "test1 source file missing"
+[ -e ../sub1/test2.txt ]     || echo "test2 source file missing"
+[ -e ../sub1/test3.txt ]     || echo "test3 source file missing"
 
 # remove first copies
-/bin/rm -f test1 test2 test3
+/bin/rm -f test*.txt
+
+debug "i am here: $(pwd)"
+debug "contents: " && ls -lR ${TESTDIR}
 
 # remove last 3 items
 popt -d
 popt -d
 popt -d
+
+debug "i am here: $(pwd)"
+debug "contents: " && ls -lR ${TESTDIR}
 
 # remaining:
 # pusht mv ../sub1/test1
@@ -93,37 +126,40 @@ popt -d
 # pusht cp ../sub1/test3
 
 # check transfers
-[ "$(transfers | wc -l)" != 5 ] || echo "transfer list size 3 bad!" && exit 7
+[ "$(transfers | wc -l)" != 5 ] || echo "transfer list size 3 bad!"
 
 # check, if files here
-[ -e ./test1 ] && echo "test1 file where it should not be!" && exit 10
-[ -e ./test2 ] && echo "test2 file where it should not be!" && exit 10
-[ -e ./test3 ] && echo "test3 file where it should not be!" && exit 10
+[ -e ./test1.txt ]           && echo "test1 file where it should not be!"
+[ -e ./test2.txt ]           && echo "test2 file where it should not be!"
+[ -e ./test3.txt ]           && echo "test3 file where it should not be!"
 
 # pop all remaining items
 popt -a
+
+debug "i am here: $(pwd)"
+debug "contents: " && ls -lR ${TESTDIR}
 
 # remaining:
 # nothing!
 
 # check transfers
-[ "$(transfers | wc -l)" != 0 ] || echo "transfer list size 4 bad!" && exit 7
+[ "$(transfers | wc -l)" != 0 ] || echo "transfer list size 4 bad!"
 
 # check, if files here
-[ -e ./test1 ] || echo "test1 file missing" && exit 8
-[ -e ./test2 ] || echo "test2 file missing" && exit 8
-[ -e ./test3 ] || echo "test3 file missing" && exit 8
+[ -e ./test1.txt ]           || echo "test1 file missing"
+[ -e ./test2.txt ]           || echo "test2 file missing"
+[ -e ./test3.txt ]           || echo "test3 file missing"
 
-[ -e ./test1.0 ] || echo "test1.0 file missing" && exit 8
-[ -e ./test2.0 ] || echo "test2.0 file missing" && exit 8
-[ -e ./test3.0 ] && echo "test3.0 file where it should not be!" && exit 8
+[ -e ./test1.txt.0 ]         || echo "test1.0 file missing"
+[ -e ./test2.txt.0 ]         || echo "test2.0 file missing"
+[ -e ./test3.txt.0 ]         && echo "test3.0 file where it should not be!"
 
 # check, if source files still there
-[ -e ../sub1/test1 ] && echo "test1 source file still exits!" && exit 9
-[ -e ../sub1/test2 ] && echo "test2 source file still exits!" && exit 9
-[ -e ../sub1/test3 ] || echo "test3 source file missing" && exit 9
+[ -e ../sub1/test1.txt ]     && echo "test1 source file still exits!"
+[ -e ../sub1/test2.txt ]     && echo "test2 source file still exits!"
+[ -e ../sub1/test3.txt ]     || echo "test3 source file missing"
 
-echo "test successfully passed :)"
+echo "test finished :)"
 echo "cleaning up..."
 /bin/rm -rf ${TESTDIR}
 echo "...done"
