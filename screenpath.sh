1f199837d8a61aa59092411ca1fc0908d2ba66bd
Jochen Issing <c.333+github@nesono.com>
2010-09-28 14:13:43 +0200

    removed obsolete line for truncation

--- screenpath.sh
+++ screenpath.sh
@@ -2,5 +2,4 @@
 # script to call repository status functions in screen's backtick
 
 SCREENPATH=$(pwd | sed 's/\/home\//~/' | sed "s/~${USER}\(\/\)\?/~\//")
-#[ ${#SCREENPATH} -gt 47 ] && SCREENPATH=..${SCREENPATH:${#SCREENPATH}-43}
 echo ${SCREENPATH}
