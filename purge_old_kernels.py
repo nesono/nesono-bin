b443d3d5861400f4d1f7bf807bf42b3c8292496a
Jochen Issing <c.333+github@nesono.com>
2012-03-30 13:58:46 +0200

    change ws to tab and add becoming root

--- purge_old_kernels.py
+++ purge_old_kernels.py
@@ -5,7 +5,7 @@
 #
 # Copyright (c) 2012, Jochen Issing <iss@nesono.com>
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #     * Redistributions of source code must retain the above copyright
@@ -16,7 +16,7 @@
 #     * Neither the name of the <organization> nor the
 #       names of its contributors may be used to endorse or promote products
 #       derived from this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -30,22 +30,36 @@
 
 import os
 import sys
+import pwd
 import re
 import operator
 from collections import defaultdict
 import apt
+from subprocess import check_call
 
 # function to calculate a key from kernel name
 def kernel_hash( match ):
-  # extract integers
-  v1 = int(match.group(1))
-  v2 = int(match.group(2))
-  v3 = int(match.group(3))
-  v4 = int(match.group(4))
-  # create version integer
-  #print v1*10**9 + v2*10**6 + v3*10**3 + v4
-  return v1*10**9 + v2*10**6 + v3*10**3 + v4
+	# extract integers
+	v1 = int(match.group(1))
+	v2 = int(match.group(2))
+	v3 = int(match.group(3))
+	v4 = int(match.group(4))
+	# create version integer
+	#print v1*10**9 + v2*10**6 + v3*10**3 + v4
+	return v1*10**9 + v2*10**6 + v3*10**3 + v4
 
+# function to get super user priveleges
+def call_as_root():
+	# we need super user priveleges for this script...
+	if not os.getuid() == 0:
+		print 'We need super user priveleges to run this script succesfully'
+		print 'Recalling this script as super user...'
+		print 'calling:','sudo '+' '.join(sys.argv)
+		check_call( 'sudo '+' '.join(sys.argv), shell=True )
+		sys.exit(0)
+
+# check if we are not root and recall this script if so
+call_as_root()
 
 # all linux kernel packages start with this string
 kernel_name_base = 'linux-image-'
@@ -67,20 +81,20 @@ installed = installed.split('\n')
 num_installed = defaultdict(list)
 # filter out kernel packages without numerics (virtual packages)
 for item in installed:
-  match = prog.match(item)
-  if match:
-    num_installed[kernel_hash(match)].append(item)
-    #print 'version:',match.group(1),match.group(2),match.group(3),match.group(4)
-    #print 'hash:   ',kernel_hash(match)
+	match = prog.match(item)
+	if match:
+		num_installed[kernel_hash(match)].append(item)
+		#print 'version:',match.group(1),match.group(2),match.group(3),match.group(4)
+		#print 'hash:   ',kernel_hash(match)
 
 sorted_installed = sorted(num_installed.iteritems(), key=operator.itemgetter(0))
 
 if len(sorted_installed) == 0:
-  print 'exiting'
-  sys.exit(0)
+	print 'exiting'
+	sys.exit(0)
 
 #for k,v in sorted_installed:
-#  print k, ' content:', v
+#	print k, ' content:', v
 #print 'num installed: ', num_installed
 
 
@@ -88,35 +102,35 @@ if len(sorted_installed) == 0:
 latest = sorted_installed[-1]
 latest_active = False
 for kernel in latest[1]:
-  if kernel == active:
-    latest_active = True
+	if kernel == active:
+		latest_active = True
 
 # check if already latest installed kernel version active
 if latest_active:
-  # active:
-  print 'active kernel IS latest installed kernel'
+	# active:
+	print 'active kernel IS latest installed kernel'
 else:
-  # not active:
-  print 'active kernel IS NOT latest installed kernel'
-  print '  active:', active
-  print '  latest:', latest[1]
-  # ask to reboot
-  print 'please reboot and restart this script'
-  sys.exit(0)
+	# not active:
+	print 'active kernel IS NOT latest installed kernel'
+	print '	active:', active
+	print '	latest:', latest[1]
+	# ask to reboot
+	print 'please reboot and restart this script'
+	sys.exit(0)
 
 # remove current kernel (and one backup) from list
 to_purge = sorted_installed[0:-2]
 if len( to_purge ) == 0:
-  print 'nothing to purge :)'
-  print 'exiting'
-  sys.exit(0)
+	print 'nothing to purge :)'
+	print 'exiting'
+	sys.exit(0)
 
 kernel_string = ''
 # put together string for debugging
 # combine kernels into string for deinstallation
 for k,v in to_purge:
-  for kernel in v:
-    kernel_string += kernel + ' '
+	for kernel in v:
+		kernel_string += kernel + ' '
 
 # ask to delete old kernels
 print 'command to purge kernels:'
@@ -124,24 +138,24 @@ print 'aptitude remove', kernel_string
 
 answer = 'cont'
 while( answer == 'cont' ):
-  answer = raw_input('Apply? [Y/n] ')
-  if answer == '':
-    answer = 'y'
-  if answer.lower()[0] == 'y':
-    break
-  if answer.lower()[0] == 'n':
-    print 'aborting by user request'
-    sys.exit(0)
-  answer = 'cont'
+	answer = raw_input('Apply? [Y/n] ')
+	if answer == '':
+		answer = 'y'
+	if answer.lower()[0] == 'y':
+		break
+	if answer.lower()[0] == 'n':
+		print 'aborting by user request'
+		sys.exit(0)
+	answer = 'cont'
 
 # get apt cache
 cache = apt.Cache()
 # mark all packages for deletion
 for k,v in to_purge:
-  for kernel in v:
-    pkg = cache[kernel]
-    pkg.mark_delete()
-    print 'package',kernel,'is marked for delete',pkg.marked_delete
+	for kernel in v:
+		pkg = cache[kernel]
+		pkg.mark_delete()
+		print 'package',kernel,'is marked for delete:',pkg.marked_delete
 
 print 'committing changes'
 # commit changes to apt
