33cd778cb6e9e15a1979b50f5df4ec158d3b30e5
Jochen Issing <c.333+github@nesono.com>
2014-12-28 23:33:35 +0100

    fix system check

--- dns_flush.sh
+++ dns_flush.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-if $(uname -r) == 14.0.0; then
+case "$(uname -r)" in
+  14.* )
 	echo "flushing unicast dns cache"
 	sudo discoveryutil udnscachestats
 	echo "flushing multicast dns cache"
 	sudo discoveryutil mdnsflushcache
 	echo "done"
-else
+	;;
+  * )
 	echo "system not supported"
-fi
+	;;
+esac
