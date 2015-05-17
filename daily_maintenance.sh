68b17a2b8817942e6f1345648d9eb7bc8edc6046
Jochen Issing <c.333+github@nesono.com>
2015-05-17 22:01:22 +0200

    cleanup comments

--- daily_maintenance.sh
+++ daily_maintenance.sh
@@ -35,28 +35,23 @@ UNAME=$(uname -s)
 
 function check_and_update_svn_bin_repo()
 {
-	# parameter 1: local svn directory
 	local DIR="${1}"
 
-	# check, if dir existent
 	if [ ! -d "${DIR}" ]; then
 		echo "skipping: ${DIR}"
 		return
 	else
-		# change into dir
 		pushd "${DIR}" &> /dev/null
 		echo "### checking if svn repo: ${DIR}"
-		# check, if dir is svn repo
+
 		local REV=$(svn info 2>/dev/null | grep Revision | sed -e 's/Revision: //')
 		[ "$REV" ] || return
 
 		echo "### checking for svn repo: ${DIR}"
 
-		# check if repo accessible
 		svn st -u &> /dev/null
 		if [ ! $? == 0 ]; then echo "### Can not access repository! Stopping update"; echo ''; return; fi
 
-		# check for remote/repo changes
 		if [ -n "$(svn st -u | grep -e '^[ ]\+\*')" ]; then
 			echo "Remote changes detected in ${DIR}!"
 			while ( true ); do
@@ -106,32 +101,28 @@ function check_and_update_svn_bin_repo()
 
 function check_and_update_git_bin_repo()
 {
-	# parameter 1: local git directory
 	local DIR="${1}"
 
 	if [ ! -d "${DIR}" ]; then
 		echo "skipping: ${DIR}"
 		return
 	else
-		# change into repo dir
 		pushd "${DIR}" &> /dev/null
 		echo "### checking if git repo: ${DIR}"
-		# check git call
+
 		STATUS=$(git status --porcelain 2>/dev/null)
 		[ $? -eq 128 ] && return
 
 		REMOTEUP=1
-		# check if remote repo (origin) is reachable
+
 		git fetch origin &> /dev/null
 		if [ ! $? == 0 ]; then REMOTEUP=0; fi
 
 		echo "### checking for git repo: ${DIR}"
 		if [ -n $(git remote | grep -e '^origin') ]; then
-			# check if remote accessible at all
 			if [ ${REMOTEUP} == 0 ]; then
 				echo "### Can not access depot! Skipping rebase"
 			else
-				# get remote/repo changes
 				git pull --rebase
 			fi
 		else
@@ -168,9 +159,9 @@ function check_and_update_git_bin_repo()
 				esac
 			done
 		fi
-		# update submodules
+
 		git submodule update --init
-		# get back to origin directory
+
 		popd &> /dev/null
 		echo ""
 	fi
@@ -178,22 +169,18 @@ function check_and_update_git_bin_repo()
 
 function check_and_update_homebrew()
 {
-	# check if brew command available
 	if [ -x "$(which brew)" ]; then
 		echo "### brew available"
 
 		echo "### running brew update"
-		# update repo
 		brew update
 
-		# show outdated ports
 		OUTDATED=$(brew outdated)
 
 		if [ -n "${OUTDATED}" ]; then
 			read -e -p "Shall I upgrade the outdated formulas? [Y/n] " ANSWER
 			case ${ANSWER} in
 				"" | "y" | "Y" )
-					# upgrade system
 					brew upgrade ${OUTDATED}
 					;;
 			esac
@@ -209,22 +196,18 @@ function check_and_update_homebrew()
 
 function check_and_update_macports
 {
-	# check for mac ports binary
 	if [ -x "$(which port)" ]; then
 		echo "### port available"
 
 		echo "### running port selfupdate"
-		# upgrade system
 		port -c selfupdate
 
-		# show outdated ports
 		OUTDATED=$(port list outdated)
 
 		if [ -n "${OUTDATED}" ]; then
 			read -e -p "Shall I upgrade the outdated ports? [Y/n] " ANSWER
 			case ${ANSWER} in
 				"" | "y" | "Y" )
-					# upgrade system
 					port -u upgrade outdated
 					;;
 			esac
@@ -240,7 +223,6 @@ function check_and_update_macports
 
 function mac_software_update_interactive()
 {
-	# ask to check for software update
 	read -e -p "Shall I check for apple softwareupdate? [y/N] " ANSWER
 
 	case ${ANSWER} in
@@ -249,11 +231,9 @@ function mac_software_update_interactive()
 			TMPSTDERR=/tmp/daily_maintenance.stderr.tmp
 
 			echo "getting list of available upgrades"
-			# get list of available upgrade
 			softwareupdate -l > ${TMPSTDOUT} 2> ${TMPSTDERR}
 			if [ -z "$(cat ${TMPSTDERR})" ]; then
 				cat ${TMPSTDOUT}
-				# ask to apply updates
 				read -e -p "Shall I apply softwareupdate? [a/r/N] " ANSWER
 
 				case ${ANSWER} in
@@ -274,7 +254,6 @@ function mac_software_update_interactive()
 
 function check_and_update_aptget()
 {
-	# upgrade system
 	echo "upgrading system"
 	apt-get update
 	apt-get -u dist-upgrade
@@ -294,10 +273,8 @@ function check_and_update_aptget()
 		echo ""
 	fi
 
-	# autoremove old pacakges
 	apt-get autoremove
 
-	# cleaning repository
 	echo "autocleaning repository"
 	apt-get autoclean
 
@@ -350,9 +327,8 @@ function check_and_update_drush()
 		drush -r /var/www pm-update
 	fi
 	if [ -n "$(jls | grep www.nesono.com)" ]; then
+		echo www.nesono.com jail found - checking for drupal updates
 		jexec 1 drush -r /usr/local/www/apache24/data/ pm-update
-	else
-		echo "www.nesono.com not found"
 	fi
 }
 
@@ -374,28 +350,19 @@ function run_in_tmux()
 }
 
 if [ "$1" != "--after-su" ]; then
-	# restart in tmux if available and not yet running
 	run_in_tmux
-	# check for nesono-bin directory and upgrade it, if neccessary - svn version
 	check_and_update_svn_bin_repo ${NESONOBININSTALLATIONDIR}
-	# check for nesono-bin directory and upgrade it, if neccessary - git version
 	check_and_update_git_bin_repo ${NESONOBININSTALLATIONDIR}
 
-	# check for underlying system
 	case ${UNAME} in
 		Darwin)
-		# Darwin (with home brew)
 		check_and_update_homebrew
 		;;
 	esac
 fi
 
 # check if sudo necessary at all
-# Darwin needs fink of macports to be installed
-# Linux needs aptitude/apt-get installed
-# reset do sudo
 do_sudo=0
-# check for underlying system
 case ${UNAME} in
 	Darwin)
 		[[ -x "$(which port)"		 ]] && do_sudo=1
@@ -411,7 +378,7 @@ case ${UNAME} in
 	;;
 esac
 
-# check for user id (must be run as root)
+# apply sudo
 if [ "${do_sudo}" == "1" ]; then
 	if [ ${EUID} != 0 ]; then
 		echo "remaining script must be run as root! Recalling with sudo"
