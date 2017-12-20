0d6993523af19c950f7784ef20566e586a13b77f
Jochen Issing <c.333+github@nesono.com>
2017-12-20 23:51:22 +0100

    small improvements on the prompt

--- fish/config.fish
+++ fish/config.fish
@@ -13,23 +13,38 @@ set __fish_git_prompt_showupstream 'yes'
 set __fish_git_prompt_color_branch yellow
 set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
+set __fish_git_prompt_showcolorhints 'yes'
 
 # Status Chars
-#set __fish_git_prompt_char_dirtystate '⚡'
-#set __fish_git_prompt_char_stagedstate '→'
-#set __fish_git_prompt_char_untrackedfiles '☡'
-#set __fish_git_prompt_char_stashstate '↩'
-#set __fish_git_prompt_char_upstream_ahead '+'
-#set __fish_git_prompt_char_upstream_behind '-'
+set __fish_git_prompt_char_upstream_prefix ''
+set __fish_git_prompt_char_upstream_ahead  '↑'
+set __fish_git_prompt_char_upstream_behind '↓'
+set __fish_git_prompt_char_stateseparator  '|'
+set __fish_git_prompt_char_dirtystate      '✚'
+set __fish_git_prompt_char_invalidstate    '✖'
+set __fish_git_prompt_char_stagedstate     '●'
+set __fish_git_prompt_char_untrackedfiles  '…'
+set __fish_git_prompt_char_cleanstate      '✔'
+set __fish_git_prompt_char_stashstate      '↩'
+#set __fish_git_prompt_describe_style 'branch'
 
 function fish_prompt
-  set last_status $status
+  set -l last_status $status
+
+  printf '%s' (echo '>')
+
+  if not test $last_status -eq 0
+    set_color $fish_color_error
+    #printf '%d' ($last_status)
+    set_color normal
+  end
+  printf '%s ' (__fish_git_prompt)
+  printf '%s ' (date "+%y-%m-%d %H:%M:%S")
+  set_color green
+  printf  '%s\n' (hostname)
 
   set_color $fish_color_cwd
-  printf '%s' (prompt_pwd)
-  set_color normal
-
-  printf '%s ' (__fish_git_prompt)
+  printf '%s\nλ ' (prompt_pwd)
 
   set_color normal
 end
