a8f31848be740cb50fadfd33d2d38c5325f8f39c
Jochen Issing <c.333+github@nesono.com>
2017-12-21 15:04:42 +0100

    cleanup fish/config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,3 @@
-set normal (set_color normal)
-set magenta (set_color magenta)
-set yellow (set_color yellow)
-set green (set_color green)
-set red (set_color red)
-set gray (set_color -o black)
-
 # Fish git prompt
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
@@ -14,20 +7,19 @@ set __fish_git_prompt_color -b grey
 set __fish_git_prompt_color_branch -b grey yellow
 set __fish_git_prompt_color_upstream_ahead -b grey green
 set __fish_git_prompt_color_upstream_behind -b grey red
-#set __fish_git_prompt_showcolorhints 'yes'
 set __fish_git_prompt_show_informative_status 'yes'
 
 # Status Chars
 set __fish_git_prompt_char_upstream_prefix ':'
-set __fish_git_prompt_char_upstream_ahead  'A'
+set __fish_git_prompt_char_upstream_ahead 'A'
 set __fish_git_prompt_char_upstream_behind 'B'
-set __fish_git_prompt_char_stateseparator  '.'
-set __fish_git_prompt_char_dirtystate      'm'
-set __fish_git_prompt_char_invalidstate    'x'
-set __fish_git_prompt_char_stagedstate     'M'
-set __fish_git_prompt_char_untrackedfiles  '?'
-set __fish_git_prompt_char_cleanstate      ''
-set __fish_git_prompt_char_stashstate      '^'
+set __fish_git_prompt_char_stateseparator '.'
+set __fish_git_prompt_char_dirtystate 'm'
+set __fish_git_prompt_char_invalidstate 'x'
+set __fish_git_prompt_char_stagedstate 'M'
+set __fish_git_prompt_char_untrackedfiles '?'
+set __fish_git_prompt_char_cleanstate ''
+set __fish_git_prompt_char_stashstate '^'
 set __fish_git_prompt_describe_style 'branch'
 
 function fish_prompt
