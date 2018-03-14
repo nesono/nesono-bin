ae4d2691cb2fa319ea5376e7e2a0b11859379487
Jochen Issing <c.333+github@nesono.com>
2018-03-14 14:37:43 +0100

    add script to download all deb packages recusively

---
+++ dpkg_download_with_deps.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ ! -d "$2" ]; then
+	echo "usage: $0 <package name> <output dir>"
+	echo " Note that the output directory needs to be created"
+	echo " The output directory may contain debian packages"
+	echo " which then will be added to the package listing"
+	exit 1
+fi
+
+package_name=$1
+output_folder=$2
+
+cd $output_folder
+
+echo "dependencies for package $package_name"
+dependencies=$(apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances --no-pre-depends $package_name | grep "^\w" | sort -u)
+
+apt-get download $dependencies
+
+dpkg-scanpackages . | gzip -9c > Packages.gz
+
+echo "Package download done"
+echo
+echo "Once you finished adding packages, please add the folder to your apt.sources.list:"
+
+echo "echo "deb file:$output_folder ./" | sudo tee -a /etc/apt/sources.list.d/$output_folder.list"
+echo
+echo "And then run"
+echo "sudo apt-get update"
+echo
