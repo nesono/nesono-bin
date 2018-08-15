0ed55e40a9f7e1303cd4e94bd128d8a729cc87ef
Jochen Issing <c.333+github@nesono.com>
2018-08-15 11:26:41 +0200

    add documentation about certinfo

--- certinfo_http.sh
+++ certinfo_http.sh
@@ -4,6 +4,8 @@
 usage()
 {
     echo "usage certinfo_http [-starttls smtp] www.example.com:25"
+    echo "On a Mac, call the following line to get validity information:"
+    echo "certinfo_http.sh www.example.com:587 -starttls smtp | grep -B 4 -A 3 Validity"
 }
 
 if [[ -z "$1" ]]; then
