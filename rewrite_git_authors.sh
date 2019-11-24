daac7aff22d989757ac75b367577850a84ea2be5
Jochen Issing <c.333+github@nesono.com>
2019-11-24 14:34:07 +0100

    Add rewrite git authors

---
+++ rewrite_git_authors.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+OLD_EMAIL=".*issing.*"
+CORRECT_NAME="Jochen Issing"
+CORRECT_EMAIL="c.333+github@nesono.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
