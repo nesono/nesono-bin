f095719c59a1053da50ac9f5abc28c80f5bec873
Jochen Issing <c.333+github@nesono.com>
2020-06-23 13:57:44 +0200

    Update display names

--- enable_cinema_display.sh
+++ enable_cinema_display.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-xrandr --output DP-2-3 --mode 1920x1200 --right-of eDP-1 --dpi 72 --scale 1.5x1.5 --panning 2880x1800+2560+0
+xrandr --output DP-1-2-2 --mode 1920x1200 --right-of eDP-1-1 --dpi 72 --scale 1.5x1.5 --panning 2880x1800+2560+0
