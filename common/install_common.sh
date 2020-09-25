83ca904eef8e5dd2fd9dbb0d48df425ed7ade51d
Jochen Issing <c.333+github@nesono.com>
2020-09-25 08:40:12 +0200

    Add comment about color codes

--- common/install_common.sh
+++ common/install_common.sh
@@ -1,20 +1,26 @@
 # include script - not for running
+
+# prefix "o    "; blue foreground, default background"
 echo_section() {
     echo -e "\e[38;5;69mo  ""$@""\e[0m"
 }
 
+# prefix "oooo"; blue foreground, default background
 echo_sub_section() {
-    echo -e "\e[38;5;69moo ""$@""\e[0m"
+    echo -e "\e[38;5;69moo\e[38;5;69moo ""$@""\e[0m"
 }
 
+# prefix "xx "; red foreground, default background
 echo_change() {
     echo -e "\e[38;5;202mxx ""$@""\e[0m"
 }
 
+# prefix "i  "; yellow foreground, default background
 echo_info() {
     echo -e "\e[38;5;227mi  ""$@""\e[0m"
 }
 
+# prefix "!! "; red background, white foreground
 echo_error() {
     echo -e "\e[97m\e[41m!! ""$@""\e[0m"
 }
