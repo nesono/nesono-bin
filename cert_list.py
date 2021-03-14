34bd6c07ed3753489d343f0c6fa0fb4709d4402d
Jochen Issing <c.333+git@nesono.com>
2021-03-14 18:09:31 +0100

    Add support for reading service and connect list from file

--- cert_list.py
+++ cert_list.py
@@ -7,6 +7,10 @@ import colorama
 from colorama import Fore, Style
 import datetime
 
+# import logging
+
+# logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.DEBUG)
+# logger = logging.getLogger(__file__)
 
 def retrieve_certinfo(hostname, port):
     ctx = ssl.create_default_context()
@@ -26,18 +30,8 @@ def retrieve_starttls_certinfo(hostname, port):
             return sslsock.getpeercert()
     return None
 
-@click.command()
-@click.option('--service', 
-        type=click.Choice(['HTTPS', 'SMTP'], 
-        case_sensitive=False), 
-        default="http", help='Service to connect to (important for starttls)')
-@click.option('--connect',
-        help='The service url to connect to in the form <address>:<port>, e.g. www.example.com:443')
-def cert_check(service, connect):
-    """Function to check the certificate expiry of a specific server"""
-    # print(f'Connecting to {service} url {connect}')
-    # print(Fore.BLUE + f'Connecting to {service} url {connect}')
-    # print(Style.RESET_ALL)
+def print_cert_check(service, connect, now=datetime.datetime.now()):
+    """Function to check and print the certificate expiry of a specific service"""
 
     if not ':' in connect:
         print(f'Connect argument does not contain a colon: {connect}')
@@ -45,21 +39,55 @@ def cert_check(service, connect):
 
     hostname, port = connect.split(':')
     port = int(port)
-    # print(f'hostname {hostname}, port: {port}')
     if service.lower() == 'https':
         cert = retrieve_certinfo(hostname, port)
     else:
         cert = retrieve_starttls_certinfo(hostname, port)
 
-    #  Nov 27 07:32:24 2020 GMT
+    #  formatted as Nov 27 07:32:24 2020 GMT
     not_before = datetime.datetime.strptime(cert['notBefore'], "%b %d %H:%M:%S %Y %Z") 
     not_after = datetime.datetime.strptime(cert['notAfter'], "%b %d %H:%M:%S %Y %Z") 
 
-    if not_after < datetime.datetime.now():
-        print(Style.RESET_ALL + Fore.RED + f'{hostname}: {not_before} - {not_after}')
+    color = Fore.GREEN
+
+    if not_after < (now + datetime.timedelta(days=8)):
+        color = Fore.YELLOW
+
+    if not_after < now:
+        color = Fore.RED
+
+    print(Style.RESET_ALL + color + f'{hostname}: {not_before} - {not_after}' + Style.RESET_ALL)
+
+
+@click.command()
+@click.option('--service', 
+        type=click.Choice(['HTTPS', 'SMTP'], 
+        case_sensitive=False), 
+        default="HTTPS", help='Service to connect to (important for starttls)')
+@click.option('--connect',
+        help='The service url to connect to in the form <address>:<port>, e.g. www.example.com:443')
+@click.option('--from_file',
+        help='Reading the service and endpoint from file line by line.\nFormat:\nHTTPS www.example.com:443',
+        type=click.File('r'))
+def main(service, connect, from_file):
+    if from_file:
+        now = datetime.datetime.now()
+
+        full_file = from_file.read(2048)
+        assert len(full_file) < 2048, "Only files up to 2047 bytes supported for now"
+
+        for line in full_file.split('\n'):
+            # file could contain empty lines
+            if len(line) == 0:
+               return 
+
+            tokens = line.strip().split(' ')
+            assert len(tokens) == 2, f"Misformed line {line}"
+
+            print_cert_check(tokens[0], tokens[1], now)
     else:
-        print(Style.RESET_ALL + Fore.GREEN + f'{hostname}: {not_before} - {not_after}')
+        print_cert_check(service, connect)
 
 
 if __name__ == '__main__':
-    cert_check()
+    main()
