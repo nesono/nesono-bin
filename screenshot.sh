e98badfcf7821c9cf8450fcc9effa605ca7b7eae
Jochen <c.333+githu@nesono.com>
2021-04-05 00:51:40 +0200

    i3 config, status, tmux fixes
    * Fix JetBrains splash screent
    * add compose key
    * fix NextCloud version
    * add screenshot tool (super+shift+4/5)
    * shorter status bar
    * tmux plugins suck

---
+++ screenshot.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+
+assert_executable() {
+    local executable=$1
+    if [ ! -x "$(which $executable 2> /dev/null)" ]; then
+        notify-send "$executable not installed, aborting screenshot"
+    fi
+}
+
+assert_executable hacksaw
+assert_executable shotgun
+assert_executable xclip
+
+MODE="full"
+while getopts ":t" opt; do
+  case ${opt} in
+    t ) 
+        MODE="tile"
+      ;;
+  esac
+done
+
+if [ "$MODE" == "tile" ]; then
+    shotgun -f png -g "$(hacksaw)" - | xclip -t 'image/png' -selection clipboard
+else
+    shotgun -f png - | xclip -t 'image/png' -selection clipboard
+fi
