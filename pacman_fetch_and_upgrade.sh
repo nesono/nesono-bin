5c9b153479ae8ddc03f3fde4920fbdd098cf7ed0
Jochen <c.333+githu@nesono.com>
2021-04-25 21:29:13 +0200

    Add echo call to remember the sequence

--- pacman_fetch_and_upgrade.sh
+++ pacman_fetch_and_upgrade.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # helper script to configure 'on_click' for i3status-rust pacman block
+echo Calling sudo pacman -Syy && sudo pacman -Syu
 sudo pacman -Syy && sudo pacman -Syu
 read -p "Script Finished. Press Enter to Quit"
