0f634755185066a8e239cf1debf4b46f0bab0d7e
Jochen <c.333+githu@nesono.com>
2021-05-08 22:16:25 +0200

    Add polybar configuration and launch script

---
+++ polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Terminate any currently running instances
+killall -q polybar
+
+# Pause while killall completes
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
+if type "xrandr" > /dev/null; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload top -c ~/.config/polybar/config &
+  done
+else
+  polybar --reload top -c ~/.config/polybar/config &
+fi
+
