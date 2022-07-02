a157f1140feb6579111672cf4fcdbe2ccff3f171
Jochen <c.333+githu@nesono.com>
2022-07-02 18:14:59 +0200

    Add some debug info and parameterized binary

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env bash
 
+echo "Polybar launching at $(date)" >> ~/.polybar_launch.log
+
+readonly polybar_bin=polybar
+
 # Terminate any currently running instances
-killall -q polybar
+killall -q $polybar_bin
 
 # Pause while killall completes
-while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+while pgrep -u $UID -x $polybar_bin > /dev/null; do sleep 1; done
 
 if type "xrandr" > /dev/null; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload top -c ~/.config/polybar/config &
+    MONITOR=$m $polybar_bin --reload top -c ~/.config/polybar/config &
   done
 else
-  polybar --reload top -c ~/.config/polybar/config &
+  $polybar_bin --reload top -c ~/.config/polybar/config &
 fi
 
+
+# fallback to i3bar if polybar does not start
+if [ -z "$(pgrep $polybar_bin)" ]; then
+    echo "Polybar ($polybar_bin) not found using pgrep: $(pgrep $polybar_bin)" >> ~/.polybar_launch.log
+    i3bar
+fi
