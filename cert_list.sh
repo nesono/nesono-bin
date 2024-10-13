3dade9662283be48b9f5d411748fd76ba6cf9566
Jochen Issing <c.333+git@nesono.com>
2024-10-13 11:39:18 +0200

    Wrap cert_list.py to ensure package installation

---
+++ cert_list.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [[ ! -r "$HOME/venv/bin/activate" ]]; then
+	python -m venv "$HOME/venv"
+	source "$HOME/venv/bin/activate"
+	pip install click colorama
+else
+	source "$HOME/venv/bin/activate"
+fi
+
+python cert_list.py --from_file "$HOME/Nextcloud/websites/cert_check_file.txt"
+
+deactivate
