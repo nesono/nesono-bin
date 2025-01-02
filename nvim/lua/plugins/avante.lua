fb84480c1d344d0a2dffbe2d27437f38cee64d8b
Jochen Issing <c.333+git@nesono.com>
2025-01-02 20:19:30 +0100

    Add avante.nvim

---
+++ nvim/lua/plugins/avante.lua
@@ -0,0 +1,22 @@
+return {
+	"yetone/avante.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"stevearc/dressing.nvim",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		{
+			"MeanderingProgrammer/render-markdown.nvim",
+			opts = { file_types = { "markdown", "Avante" } },
+			ft = { "markdown", "Avante" },
+		},
+	},
+	build = "make",
+	opts = {
+		provider = "openai",
+		openai = {
+			endpoint = "https://api.openai.com/v1/",
+			model = "gpt-4o-mini",
+		},
+	},
+}
