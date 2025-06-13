b46b0131cd77987c961ba87e19f62b70c045a877
Jochen Issing <c.333+github@nesono.com>
2025-06-13 07:57:09 +0200

    Remove gp.lua

--- nvim/lua/plugins/gp.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"robitx/gp.nvim",
-    config = function()
-        local conf = {
-			openai_api_key = os.getenv("OPENAI_API_KEY"),
-        }
-        require("gp").setup(conf)
-    end,
-}
