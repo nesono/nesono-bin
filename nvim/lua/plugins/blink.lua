78f1c5847ae7eed6ad5b4b054de69d34d69aa95f
Jochen Issing <c.333+git@nesono.com>
2025-07-06 15:07:49 +0200

    Fix blink.cmp auto complete

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,5 +1,22 @@
 return {
-  'saghen/blink.cmp',
-  -- optional: provides snippets for the snippet source
-  dependencies = { 'rafamadriz/friendly-snippets' }
+	'saghen/blink.cmp',
+	dependencies = { 'rafamadriz/friendly-snippets' },
+
+	version = '1.*',
+
+	opts = {
+		keymap = { preset = 'default' },
+
+		appearance = {
+			nerd_font_variant = 'mono'
+		},
+
+		-- Default list of enabled providers defined so that you can extend it
+		-- elsewhere in your config, without redefining it, due to `opts_extend`
+		sources = {
+			default = { 'lsp', 'path', 'snippets', 'buffer' },
+		},
+		fuzzy = { implementation = "prefer_rust_with_warning" }
+	},
+	opts_extend = { "sources.default" },
 }
