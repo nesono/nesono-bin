c56cb8d690bd5abcf1978cc114b53d662c864cd3
Jochen Issing <c.333+github@nesono.com>
2025-07-24 21:35:19 +0200

    Remove shortcuts - useless given the existing ones

--- nvim/lua/plugins/dashboard-nvim.lua
+++ nvim/lua/plugins/dashboard-nvim.lua
@@ -6,12 +6,12 @@ return {
 			config = {
 				week_header = { enable = true },
 				shortcut = {
-					{ desc = "Update Plugins", group = "@property", action = "Lazy update", key = "u" },
-					{ desc = "  Load Last Session", group = "@property", action = "SessionLoad", key = "l" },
-					{ desc = "  New File", group = "@property", action = "enew", key = "n" },
-					{ desc = "  Find Files", group = "@property", action = "Telescope find_files", key = "f" },
-					{ desc = "  Recent Files", group = "@property", action = "Telescope oldfiles", key = "r" },
-					{ desc = "  Grep", group = "@property", action = "Telescope live_grep", key = "w" },
+					-- { desc = "Update Plugins", group = "@property", action = "Lazy update", key = "u" },
+					-- { desc = "  Load Last Session", group = "@property", action = "SessionLoad", key = "l" },
+					-- { desc = "  New File", group = "@property", action = "enew", key = "n" },
+					-- { desc = "  Find Files", group = "@property", action = "Telescope find_files", key = "f" },
+					-- { desc = "  Recent Files", group = "@property", action = "Telescope oldfiles", key = "r" },
+					-- { desc = "  Grep", group = "@property", action = "Telescope live_grep", key = "w" },
 				},
 			},
 		})
