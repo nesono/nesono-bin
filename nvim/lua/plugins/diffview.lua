903874dcb095368ce43c999608d3ac9c95cf9a41
Jochen Issing <c.333+github@nesono.com>
2025-10-08 10:07:05 +0200

    Make diff view a bit more lazy

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -1,3 +1,8 @@
 return {
 	"sindrets/diffview.nvim"
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	cmd = { "DiffviewOpen", "DiffviewFileHistory", "DiffviewClose" },
+	config = function()
+		require("diffview").setup({ watch_index = true })
+	end,
 }
