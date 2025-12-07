13a1cf2484ad10dd485770800824e29a6f605d8b
Jochen Issing <c.333+git@nesono.com>
2025-12-07 08:47:21 +0100

    Remove telescope dependency and use snacks instead

--- nvim/lua/plugins/octo.lua
+++ nvim/lua/plugins/octo.lua
@@ -2,11 +2,12 @@ return {
 	"pwntester/octo.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope.nvim",
     "nvim-tree/nvim-web-devicons",
   },
   config = function ()
-    require('octo').setup()
+	  require('octo').setup({
+		  picker = "snacks",
+	  })
 	vim.api.nvim_set_keymap('n', '<leader>oo', ':Octo pr open<cr>', {noremap = true, silent = true})
 	vim.api.nvim_set_keymap('n', '<leader>ol', ':Octo pr list<cr>', {noremap = true, silent = true})
 	vim.api.nvim_set_keymap('n', '<leader>os', ':Octo search repo:fernride/talos_project is:open is:pr label:n4_image_manifest_change -is:draft base:main<cr>', {noremap = true, silent = true})
