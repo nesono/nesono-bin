b92d1d71b07361dc69911973f9d5771e1596a7b9
Jochen Issing <github@nesono.com>
2026-01-18 22:05:54 +0100

    Adding fzf-lua again
    If snacks is not supported as a picker

---
+++ nvim/lua/plugins/fzf.lua
@@ -0,0 +1,12 @@
+return {
+  "ibhagwan/fzf-lua",
+  -- optional for icon support
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  -- or if using mini.icons/mini.nvim
+  -- dependencies = { "nvim-mini/mini.icons" },
+  ---@module "fzf-lua"
+  ---@type fzf-lua.Config|{}
+  ---@diagnostic disable: missing-fields
+  opts = {}
+  ---@diagnostic enable: missing-fields
+}
