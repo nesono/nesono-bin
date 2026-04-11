974a7163c8cc3bdf7fe2cc33794efdc775afd89b
nesono <nesono@users.noreply.github.com>
2026-04-11 22:48:55 +0200

    minor cleanups and diffview keymaps (#27)

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -4,5 +4,18 @@ return {
 	cmd = { "DiffviewOpen", "DiffviewFileHistory", "DiffviewClose" },
 	config = function()
 		require("diffview").setup({ watch_index = true })
+		vim.keymap.set("n", "<leader>dp", "<cmd>DiffviewOpen origin/main...HEAD<cr>", {
+			desc = "PR diff (merge-base)",
+		})
+		vim.keymap.set("n", "<leader>df", "<cmd>DiffviewFileHistory %<cr>", {
+			desc = "Current file git history",
+		})
+		vim.keymap.set("n", "<leader>dd", function()
+			local keys = vim.api.nvim_replace_termcodes(":DiffviewFileHistory ", true, false, true)
+			vim.fn.feedkeys(keys, "n")
+		end, { desc = "Diffview (prompt)" })
+		vim.keymap.set("n", "<leader>dc", "<cmd>DiffviewClose<cr>", {
+			desc = "Close diff view",
+		})
 	end,
 }
