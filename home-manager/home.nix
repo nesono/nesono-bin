430b4696b00410827fe50245dbccd931be57d68e
nesono <github@nesono.com>
2026-07-10 14:09:55 +0200

    Add obsidian

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 
 {
+  # Required for Obsidian
+  nixpkgs.config.allowUnfree = true;
+
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "ji";
@@ -26,6 +29,7 @@
     (writeShellScriptBin "opencode" ''
       exec ${nodejs}/bin/npx -y opencode-ai@latest "$@"
     '')
+	obsidian
     planify
     playerctl
     thunderbird
