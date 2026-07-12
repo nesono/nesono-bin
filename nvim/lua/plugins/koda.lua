a1c0005c2a908c57191cb063b119f4b0185d30bc
nesono <github@nesono.com>
2026-07-12 09:37:05 +0200

    Remove unused plugins

--- nvim/lua/plugins/koda.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "oskarnurm/koda.nvim",
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    -- require("koda").setup({ transparent = true })
-    -- vim.cmd("colorscheme koda")
-  end,
-}
