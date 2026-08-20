81c8ebf41aad000ab72e0ace6fdf4297341c0e24
Jochen Issing <jochen.issing@fernride.com>
2026-08-20 23:33:18 +0200

    Add OSC52 additions

--- nvim/lua/plugins/osc52.lua
+++ nvim/lua/plugins/osc52.lua
@@ -1,8 +1,16 @@
 return {
 	"ojroques/nvim-osc52",
 	config = function()
-		vim.keymap.set("n", "<leader>y", require("osc52").copy_operator, { expr = true, desc = "Copy to clipboard" })
-		vim.keymap.set("n", "<leader>yy", "<leader>c_", { remap = true, desc = "Copy to clipboard" })
+		vim.keymap.set("n", "<leader>yy", require("osc52").copy_operator, { expr = true, desc = "Copy to clipboard" })
 		vim.keymap.set("v", "<leader>y", require("osc52").copy_visual, { desc = "Copy to clipboard" })
+		vim.keymap.set("n", "<leader>yf", function()
+          vim.fn.setreg("+", vim.fn.expand("%:p:."))
+        end, { desc = "Yank buffer relative path" })
+		vim.keymap.set("n", "<leader>yt", function()
+          vim.fn.setreg("+", vim.fn.expand("%:t"))
+        end, { desc = "Yank buffer filename" })
+		vim.keymap.set("n", "<leader>yp", function()
+          vim.fn.setreg("+", vim.fn.expand("%:p"))
+        end, { desc = "Yank buffer filename" })
 	end,
 }
